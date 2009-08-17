350b00733c0c5892ffe7499213dd041de72101e5
Timo Stollenwerk <timo@zmag.de>
2009-08-17 11:08:29 +0000

    README and description changed.
    svn path=/collective.autoresizetextarea/trunk/; revision=94923

--- README.txt
+++ README.txt
@@ -1,6 +1,10 @@
 Introduction
 ============
 
+collective.autoresizetextarea is a jQuery textarea auto-resizer for Plone.
+
+It is based on James Padolsey's jQuery autoResize plugin.
+
 http://james.padolsey.com/javascript/jquery-plugin-autoresize/
 
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '1.0'
 
 setup(name='collective.autoresizetextarea',
       version=version,
-      description="Plone integration for jQuery autoResize plugin.",
+      description="jQuery textarea auto-resizer.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
